0ed5e60c449cb7fbc479977a84bb531db86bbd62
quaziii9 <quaziii9gamedev@gmail.com>
2024-06-07 19:33:05 +0900

    [feat] SHOW RoomangerGUI 해제

--- Assets/Bug/FPSRoomPlayer.cs
+++ Assets/Bug/FPSRoomPlayer.cs
@@ -13,18 +13,21 @@ public class FPSRoomPlayer : NetworkRoomPlayer
     {
         // 로컬 플레이어가 시작될 때 서버에 자신의 닉네임을 보냅니다.
         CmdSetPlayerName(Database.Instance.PlayerNickName);
+        Debug.Log($"OnStartLocalPlayer: Sending PlayerName: {Database.Instance.PlayerNickName}");
     }
 
     [Command]
     void CmdSetPlayerName(string playerName)
     {
         PlayerName = playerName;
+        Debug.Log($"CmdSetPlayerName: PlayerName set to {playerName}");
     }
 
     private void OnPlayerNameChanged(string oldName, string newName)
     {
         // 모든 클라이언트에서 닉네임이 변경될 때 UI를 업데이트합니다.
         SetPlayerNameText();
+        Debug.Log($"OnPlayerNameChanged: PlayerName changed from {oldName} to {newName}");
     }
 
     private void SetPlayerNameText()
@@ -38,11 +41,14 @@ public class FPSRoomPlayer : NetworkRoomPlayer
         {
             NickName.text = PlayerName;
         }
+        Debug.Log($"SetPlayerNameText: NickName text set to {PlayerName}");
     }
 
     private void Start()
     {
+        // 닉네임 텍스트 업데이트
         SetPlayerNameText();
+        Debug.Log("Start: Setting PlayerName text");
     }
 
     private void OnDestroy()
