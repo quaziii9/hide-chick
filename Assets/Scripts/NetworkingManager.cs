5e19129499dfbaa02b0f001f86e59a3e8eb0081b
quaziii9 <quaziii9gamedev@gmail.com>
2024-06-07 11:41:19 +0900

    [feat] 로그인 실패 시 로그인 popup 다시 띄워주기

--- Assets/Scripts/NetworkingManager.cs
+++ Assets/Scripts/NetworkingManager.cs
@@ -3,11 +3,21 @@ using Mirror;
 
 public class NetworkingManager : NetworkManager
 {
-
+    [SerializeField] LoginPopup _loginPopup;
 
     public void OnInputValueChanged_SetHostName(string hostName)
     {
         this.networkAddress = hostName;
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+
+        if (_loginPopup != null)
+        {
+            _loginPopup.SetUIOnClientDisconnected();
+        }
     }
 
 }
