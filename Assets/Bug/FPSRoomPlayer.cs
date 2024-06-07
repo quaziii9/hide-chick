59d2b23e407dd1ac8f866a266599d267fd463052
quaziii9 <quaziii9gamedev@gmail.com>
2024-06-07 19:07:46 +0900

    [BugFix] 로컬 및 호스트에서 각자 지정한 닉네임으로 표시
    각자

--- Assets/Bug/FPSRoomPlayer.cs
+++ Assets/Bug/FPSRoomPlayer.cs
@@ -1,42 +1,52 @@
 using Mirror;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class FPSRoomPlayer : NetworkRoomPlayer
 {
-    [SyncVar]
+    [SyncVar(hook = nameof(OnPlayerNameChanged))]
     public string PlayerName;
 
     public TextMeshProUGUI NickName;
 
-    public override void OnStartServer()
+    public override void OnStartLocalPlayer()
     {
-        PlayerName = Database.Instance.PlayerNickName;
-        NickName.text = Database.Instance.PlayerNickName;
+        // 로컬 플레이어가 시작될 때 서버에 자신의 닉네임을 보냅니다.
+        CmdSetPlayerName(Database.Instance.PlayerNickName);
     }
 
+    [Command]
+    void CmdSetPlayerName(string playerName)
+    {
+        PlayerName = playerName;
+    }
+
+    private void OnPlayerNameChanged(string oldName, string newName)
+    {
+        // 모든 클라이언트에서 닉네임이 변경될 때 UI를 업데이트합니다.
+        SetPlayerNameText();
+    }
+
+    private void SetPlayerNameText()
+    {
+        if (NickName == null)
+        {
+            NickName = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        if (NickName != null)
+        {
+            NickName.text = PlayerName;
+        }
+    }
 
     private void Start()
     {
-        base.Start();
-        PlayerName = Database.Instance.PlayerNickName;
-        NickName.text = Database.Instance.PlayerNickName;
-
-        //LobbyUIManager.Instance.GameRoomPlayerCounter.UpdatePlayerCount();
-
-        //if (isServer)
-        //{
-        //    LobbyUIManager.Instance.ActiveStartButton();
-        //}
+        SetPlayerNameText();
     }
 
     private void OnDestroy()
     {
-        //if (LobbyUIManager.Instance != null)
-        //{
-        //    LobbyUIManager.Instance.GameRoomPlayerCounter.UpdatePlayerCount();
-        //}
+        // 필요에 따라 추가 코드 작성
     }
 }
